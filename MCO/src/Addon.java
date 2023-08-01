15ff154579f1259647363b249f2bfb012eb65565
LapasaranJoemar <joemar_lapasaran_a@dlsu.edu.ph>
2023-08-01 11:49:51 +0800

    added ingredient classes

---
+++ MCO/src/Addon.java
@@ -0,0 +1,8 @@
+public class Addon extends Item {
+
+    public Addon(String name, int calories, int price) {
+        super(name, calories, price);
+        //TODO Auto-generated constructor stub
+    }
+
+}
---
+++ MCO/src/Rice.java
@@ -0,0 +1,8 @@
+public class Rice extends Item {
+
+    public Rice(String name, int calories, int price) {
+        super(name, calories, price);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
