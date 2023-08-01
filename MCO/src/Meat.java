771ba4edf56f79a6ad0c5ab33a9a99bfad093dbb
LapasaranJoemar <joemar_lapasaran_a@dlsu.edu.ph>
2023-08-01 11:26:25 +0800

    testing other class

---
+++ MCO/src/Meat.java
@@ -0,0 +1,8 @@
+public class Meat extends Item {
+
+    public Meat(String name, int calories, int price) {
+        super(name, calories, price);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
